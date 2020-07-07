00e3d0084579371cd4084b509fb53a3c4781fc8c
marlene legrand <marlene.legrand8@gmail.com>
2020-07-07 13:25:01 +0200

    modification bdd

--- MySQL/database_ecommerce.sql
+++ MySQL/database_ecommerce.sql
@@ -17,12 +17,11 @@ create table commandes(id_commande int NOT NULL AUTO_INCREMENT, date_commande da
 						constraint pk_commande primary key (id_commande),
                         constraint fk_client foreign key (client_id) references clients(id_client));
                         
-create table lignes_commandes (id_ligne_commande int NOT NULL AUTO_INCREMENT, quantite int, prix double, produit_id int, commande_id int,
+create table panier(id_panier int not null auto_increment, 
+					constraint pk_panier primary key (id_panier));
+                        
+create table lignes_commandes (id_ligne_commande int NOT NULL AUTO_INCREMENT, quantite int, prix double, produit_id int, commande_id int, panier_id int,
 							   constraint pk_ligne_commande primary key (id_ligne_commande),
                                constraint fk_commande_ligne foreign key (commande_id) references commandes(id_commande),
-                               constraint fk_commande_produit foreign key (produit_id) references produits(id_produit) on delete cascade on update cascade);
-                        
-create table panier(id_panier int not null auto_increment, ligne_id int,
-					constraint pk_panier primary key (id_panier),
-                    constraint fk_panier foreign key (ligne_id) references lignes_commandes(id_ligne_commande));
-                    
+                               constraint fk_commande_produit foreign key (produit_id) references produits(id_produit) on delete cascade on update cascade,
+                               constraint fk_commande_panier foreign key (panier_id) references panier(id_panier));
